68398f6eb9d1140af37a004274b8d8b230a7cfc2
aayman01 <ayman.alfi098@gmail.com>
2024-06-05 17:20:00 +0600

    deleteOne on cart api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -109,6 +109,13 @@ async function run() {
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
+
+    app.delete('/carts/:id',async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
     //products api
     app.get("/products", async (req, res) => {
